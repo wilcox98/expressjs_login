af55c039235490143a9e39dc96ffccff126cb3a0
w1lC0x <w1lc0x@localhost.localdomain>
2018-03-02 01:15:25 -0500

    -deploy

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 var express = require('express');
+var  session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require ('mongoose');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
+var helmet = require('helmet');
+var mongo = require('mongodb')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+//connecting to mongo
+mongoose.connect('mongodb://localhost/express');
 // view engine setup
 app.engine('html' ,require('ejs').renderFile);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(helmet())
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+      secret: 'wilcox'
+     
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', index);
-app.use('/users', users);
+var routes = require('./routes/index');
+app.use('/' ,routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var User = require('../models/user');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.post('/login', function (req , res) {
+	// body...
+	if (req.body.username &&
+		req.body.password) {
+		User.authenticate(req.body.username, req.body.password), function (error, user) {
+			// body...
+			if (error || !user) {
+				var err = new Error ('Wrong Username or Password');
+				err.status = 401 ;
+				return next (err);
+				} else {
+					req.session.userId = user_.id;
+					return res.render('/profile');
+				}
+		}
+	}
+});
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' });
+});
+
+router.post('/signup', function ( res, req ) {
+	// body...
+	if (req.body.email &&
+  req.body.username &&
+  req.body.password &&
+  req.body.passwordConf) {
+
+  var userData = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    passwordConf: req.body.passwordConf,
+  }
+
+  //use schema.create to insert data into the db
+  User.create(userData, function (err, user) {
+    if (err) {
+      return next(err)
+    } else {
+      return res.redirect('/profile');
+    }
+  });
+}
+});
+
+	
 module.exports = router;
